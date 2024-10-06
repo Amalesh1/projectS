04efdf4ad64ac789a4d883866bf4e3e73a3acfd1
Amalesh1 <amaleshadhikary500@gmail.com>
2024-10-06 19:47:12 +0530

    changed few files

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/projectS/',
   plugins: [react()],
   server: {
     host: true, // Allows access from local network
